4fd0bb9b3ee05c7ede5ed97db068ffc7a918abb6
Yan-CodeSpring <2095455730@qq.com>
2026-01-24 15:10:36 +0800

    bcss512跑完了，但是val_loss竟然是一直上涨的趋势。接下来打算在busi的task里加入注意力机制。

--- bcss_train.sh
+++ bcss_train.sh
@@ -3,14 +3,14 @@ CUDA_VISIBLE_DEVICES="0" \
 python bcss_train.py \
 --hiera_path "/root/SAM2-UNet-main/sam2_hiera_large.pt" \
 --dataset_type "bcss" \
---size 224 \
---train_image_path "/root/autodl-tmp/BCSS/BCSS_224/train" \
---train_mask_path "/root/autodl-tmp/BCSS/BCSS_224/train_mask" \
---val_image_path "/root/autodl-tmp/BCSS/BCSS_224/val" \
---val_mask_path "/root/autodl-tmp/BCSS/BCSS_224/val_mask" \
---save_path "/root/SAM2-UNet-main/output_bcss_checkpoints1" \
---log_path "/root/SAM2-UNet-main/output_bcss_checkpoints1/train_log.csv" \
+--size 512 \
+--train_image_path "/root/autodl-tmp/BCSS/BCSS_512/train_512" \
+--train_mask_path "/root/autodl-tmp/BCSS/BCSS_512/train_mask_512" \
+--val_image_path "/root/autodl-tmp/BCSS/BCSS_512/val_512" \
+--val_mask_path "/root/autodl-tmp/BCSS/BCSS_512/val_mask_512" \
+--save_path "/root/SAM2-UNet-main/output_bcss_512_checkpoints1" \
+--log_path "/root/SAM2-UNet-main/output_bcss_512_checkpoints1/train_log.csv" \
 --epoch 25 \
 --lr 0.0002 \
---batch_size 64 \
+--batch_size 12 \
 --weight_decay 5e-4
--- plot_curves.sh
+++ plot_curves.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 可视化训练曲线
 python plot_curves.py \
---log_path "/root/SAM2-UNet-main/output_bcss_checkpoints1/train_log.csv" \
---save_path "/root/SAM2-UNet-main/output_bcss_checkpoints1/training_curves.png"
+--log_path "/root/SAM2-UNet-main/output_bcss_512_checkpoints1/train_log.csv" \
+--save_path "/root/SAM2-UNet-main/output_bcss_512_checkpoints1/training_curves.png"
