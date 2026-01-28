4c464e9ddea208731adcaf30da2ff753f632830f
Yan-CodeSpring <2095455730@qq.com>
2026-01-28 15:01:00 +0800

    加入了cbam模块测试， 反而下降了。现在这一版是原版的网络结构，因为cbam模块的加入是在另一个显卡上面跑的。

--- eval.sh
+++ eval.sh
@@ -1,7 +1,6 @@
 python eval.py \
---dataset_name "bcss_224_output5" \
---pred_path "/root/autodl-tmp/BCSS/output5_predict_results/" \
+--dataset_name "bcss_224_output9" \
+--pred_path "/root/autodl-tmp/BCSS/output9_predict_results/" \
 --gt_path "/root/autodl-tmp/BCSS/BCSS_224/val_mask_binary"
 
 
-ls /root/autodl-tmp/BCSS/output5_predict_results/*.{png,jpg,jpeg,bmp} | head -10
